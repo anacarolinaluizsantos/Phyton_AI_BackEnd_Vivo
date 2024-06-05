198df98f024d35ba8a22243d47e1fcca5b3027e6
anacarolinaluizsantos <65691449+anacarolinaluizsantos@users.noreply.github.com>
2024-06-05 18:06:10 -0300

    Indentação_Blocos
    Mostra como funciona o alinhamento em blocos.

---
+++ Indentacao_Blocos.py
@@ -0,0 +1,14 @@
+def sacar(valor):
+    saldo = 1000
+
+    if saldo >= valor:
+        print("Valor sacado!")
+        print("Retire o dinheiro na boca do caixa.")
+
+        print("Obrigado(a) por ser nosso cliente!!! Tenha um bom dia!!!")
+
+sacar(100)
+
+def depositar(valor):
+    saldo = 500
+    saldo+= valor
